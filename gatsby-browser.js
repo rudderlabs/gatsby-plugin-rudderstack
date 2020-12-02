d0d25e363984f01c83ad0359e3138040e77aac18
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-12-02 14:13:48 -0700

    add segment plugin code to project

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,26 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-// You can delete this file if you're not using it
+"use strict";
+
+exports.onRouteUpdate = function (_ref, _ref2) {
+  var prevLocation = _ref.prevLocation;
+  var trackPage = _ref2.trackPage,
+      _ref2$trackPageDelay = _ref2.trackPageDelay,
+      trackPageDelay = _ref2$trackPageDelay === undefined ? 50 : _ref2$trackPageDelay;
+
+  function trackSegmentPage() {
+    var delay = Math.max(0, trackPageDelay);
+
+    window.setTimeout(function () {
+      if (trackPage) {
+        window.analytics && window.analytics.page(document.title);
+      }
+    }, delay);
+  }
+
+  if (prevLocation && window.segmentSnippetLoaded === false) {
+    window.segmentSnippetLoader(function () {
+      trackSegmentPage();
+    });
+  } else {
+    trackSegmentPage();
+  }
+};
