84b477391213b0030c600e7e76d74bb4c3d888aa
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2021-01-08 17:18:24 -0800

    remove uneccessary function

--- utils/loadConfiguration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function loadConfiguration(writeKey, dataPlaneUrl, controlPlaneUrl) {
-  if (controlPlaneUrl) {
-    return "'" + writeKey + "', '" + dataPlaneUrl + "', {configUrl: '" + controlPlaneUrl + "'}";
-  } else {
-    return "'" + writeKey + "', '" + dataPlaneUrl + "'";
-  }
-}
