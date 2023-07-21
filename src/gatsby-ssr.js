cb4f7bc1888e595805f46a00fade82d18805dc6e
George Bardis <gbardis@rudderstack.com>
2023-07-21 20:00:25 +0300

    feat: add ability to switch between versions

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -12,6 +12,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
     loadType,
     sdkURL = "https://cdn.rudderlabs.com/beta/3.0.0-beta",
     loadOptions = {},
+    useLegacySDK,
   } = pluginOptions;
 
   // ensures RudderStack production write key is present
@@ -35,22 +36,27 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
 
   const loadConfig = `'${writeKey}', '${dataPlaneUrl}', ${JSON.stringify(finalLoadOptions)}`;
 
-  // let scriptTagStr = `var s = document.createElement("script");
-  //   s.type = "text/javascript";
-  //   s.src = "${sdkURL}";`;
-  // if (loadType === "async") {
-  //   scriptTagStr += "s.async = true;";
-  // } else if (loadType === "defer") {
-  //   scriptTagStr += "s.defer = true;";
-  // }
-  // scriptTagStr += "document.head.appendChild(s);";
-
-  const snippet = `var sdkBaseUrl="${sdkURL}";var sdkName="rsa.min.js";var asyncScript=true;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[];
+  let snippet =  `var sdkBaseUrl="${sdkURL}";var sdkName="rsa.min.js";var asyncScript=true;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[];
   var e=["setDefaultInstanceKey","load","ready","page","track","identify","alias","group","reset","getAnonymousId","setAnonymousId","startSession","endSession","getSessionId"];
   for(var n=0;n<e.length;n++){var d=e[n];window.rudderanalytics[d]=function(e){return function(){window.rudderanalytics.push([e].concat(Array.prototype.slice.call(arguments)))}}(d)}
   try{new Function('return import("")'),window.rudderAnalyticsBuildType="modern"}catch(a){}
   if(window.rudderAnalyticsMount=function(){var e=document.createElement("script");e.src="".concat(sdkBaseUrl,"/").concat(window.rudderAnalyticsBuildType,"/").concat(sdkName),e.async=asyncScript,document.head?document.head.appendChild(e):document.body.appendChild(e)},"undefined"==typeof Promise){var t=document.createElement("script");
   t.src="https://polyfill.io/v3/polyfill.min.js?features=globalThis%2CPromise&callback=rudderAnalyticsMount",t.async=asyncScript,document.head?document.head.appendChild(t):document.body.appendChild(t)}else{window.rudderAnalyticsMount()`;
+
+  if (useLegacySDK) {
+    let scriptTagStr = `var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.src = "${sdkURL}";`;
+    if (loadType === "async") {
+      scriptTagStr += "s.async = true;";
+    } else if (loadType === "defer") {
+      scriptTagStr += "s.defer = true;";
+    }
+    scriptTagStr += "document.head.appendChild(s);";
+
+    snippet = `rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}
+    ${scriptTagStr}`;
+  }
 
   const instantLoader = `${snippet}${manualLoad ? `` : `rudderanalytics.load(${loadConfig})`};`;
 
